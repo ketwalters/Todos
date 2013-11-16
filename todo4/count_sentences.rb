0ad48db6c892172a024754d393d623581bfb89bb
Keturah Walters <your.ketwalters@gmail.com>
2013-11-15 23:19:48 -0500

    add modified count_sentences.rb file

---
+++ todo4/count_sentences.rb
@@ -0,0 +1,10 @@
+# Write a method on String called `count_sentences` that returns the number of
+# sentences in the string it is called on
+
+class String
+  def count_sentences
+  	count "." "?"
+  end
+end
+
+
