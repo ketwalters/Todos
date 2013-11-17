9ad4fd3bdf513362361c55c692c00709155ec0d6
Keturah Walters <your.ketwalters@gmail.com>
2013-11-16 22:17:35 -0500

    add modified blackjack.rb file

--- games/blackjack.rb
+++ games/blackjack.rb
@@ -12,68 +12,38 @@
 ##Challenge
 #After successfully completing the previous game (there should be a commit with a working version of this game), alter the game so that a player can "hit" as many times as they want.
 
-
-def deal
-  rand(1..11)
+def deal_card
+    card_array = [1,2,3,4,5,6,7,8,9,10,11]
+    card_1 = card_array[rand(11)]
+    puts card_1
+	card_2 = card_array[rand(11)]
+    puts card_2
+    total = card_1 + card_2
+    puts total
 end
+deal_card
 
-def question
-  puts "Do you want to play(y/n)"
-end
 
-def calculate score, hit = 0
-  if score == 21
-    puts "BlackJack"
-  elsif score > 21 || hit == 3
-    puts "You lose"
-  end
-end
+puts "Do you want to hit or stay?"
+    answer = gets.chomp
+    card_3 = card_array[rand(11)]
+if answer == "hit" 
+    puts card_3 
+    puts card_3 + total
+    total_2 = card_3 + total
+    puts total_2
+    
 
-puts "Hello Welcome to Blackjack Game"
-question()
-choice = gets.chomp 
-
-while choice == 'y'
-  hit = 0
-  card1 = deal()
-  card2 = deal()
-  score = card1 + card2
-  
-  puts "Card1: #{card1}"
-  puts "Card2: #{card2}"
-  puts "Score: #{score}"
-  puts "Current hit: #{hit}"
-  calculate(score)
-  
-
-  if score == 21
-    hit = 2
-  end
-
-  while hit < 2 && score < 21
-    puts "Do you want to hit or stay?"
-    p1_res = gets.chomp
-    if p1_res == 'hit'
-      hit_card = deal()
-      score = score + hit_card
-      calculate(score, hit)
-    else
-      break
+puts "Do you want another hit?"
+    answer_2 = gets.chomp
+    card_4 = card_array[rand(11)]
+    if answer_2 == "yes"
+    puts card_4 
+    total_3 = card_4 + total_2
+    puts total_3
+    else answer_2 == "stay"
     end
-    hit = hit + 1
-    puts "current score: #{score}"
-    puts "current hit: #{hit}"
-  end
-
-  if score < 21
-    puts "You lose"
-  end
-
-  question()
-  choice = gets.chomp
 end
-
-puts "Good-bye, Thank you for playing"
 
 
 
