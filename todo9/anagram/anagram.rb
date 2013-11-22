64e1b98c261f8af191e921408f62c38fc57fc9d0
Keturah Walters <your.ketwalters@gmail.com>
2013-11-22 09:05:33 -0500

    add anagram.rb, anagram.spec files

---
+++ todo9/anagram/anagram.rb
@@ -0,0 +1,5 @@
+class Anagram
+
+	def anagram
+	end
+end
---
+++ todo9/anagram/anagram_spec.rb
@@ -0,0 +1,8 @@
+RSpec.configure do |config|
+	config.color_enabled = true
+	config.tty = true
+	config.formatter = :progress
+end
+
+	require_relative './anagram'
+
