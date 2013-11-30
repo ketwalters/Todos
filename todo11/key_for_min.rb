8af72f93be80a15ff91d5b8c3ced433bb3d5c691
Keturah Walters <your.ketwalters@gmail.com>
2013-11-29 20:22:21 -0500

    Add updated method

--- todo11/key_for_min.rb
+++ todo11/key_for_min.rb
@@ -1,11 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+hash1 = {:blake => 1, :ashley => 200, :adam => 2}
 def key_for_min_value(hash)
-	container = {}
-	hash.each do |key, value|
-		if value.min 
-			container << key
-  	end
-  end
+	hash.values.min
 end
-
+puts key_for_min_value(hash1)
