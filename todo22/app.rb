906f5edf9ae43f83d12f6d9b1d6865c68e03759b
Keturah Walters <your.ketwalters@gmail.com>
2013-12-29 16:42:34 -0500

    add is it my birthday or christmas

--- todo22/app.rb
+++ todo22/app.rb
@@ -1,14 +1,30 @@
 require 'bundler'
 Bundler.require
 
-class Birthday < Sinatra::Application
+class App < Sinatra::Application
 
-	get '/:birthday' do
-		@birthday = params[:birthday]
-		if @birthday == 12/19
-			puts "Yes"
+	get '/' do
+		t = Time.now
+		if t.month == 10 && t.day == 9
+			@answer = "Yes"
 		else
-			puts "No"
+			@answer = "No"
+			erb :birthday
 		end
 	end
+
+	get'/:holiday' do
+		t = Time.now
+		@holiday = params[:holiday]
+		if @holiday == "Christmas"
+			if t.month == 12 && t.day == 25
+			@christmas_answer = "Yes"
+		else
+			@christmas_answer = "No"
+			erb :results
+		end
+		end
+	end
+
+
 end
