b6b12ae18bc408acd322b406410581124b32330e
Keturah Walters <your.ketwalters@gmail.com>
2013-12-29 00:11:18 -0500

    add Array.uniq

--- Todo21/unique.rb
+++ Todo21/unique.rb
@@ -3,15 +3,21 @@
 class Array
 
   def uniq(array)
-  	hash = Hash.new(0)
+    container = []
+    hash = Hash.new(0)
     array.each do |num|
-    	hash[num] += 1
+      hash[num] = array.count(num)
+      hash.each do |k, count|
+        if count >= 2
+          container << array.delete(num)
+          array << container
+          array.flatten!
+        end
+      end
     end
-    hash.each do |key, count|
-    	if count >= 2 
-    		key.clear
-
-
   end
 
 end
+
+a = Array.new
+a.uniq([1,2,2,3])
