2e640c8645ef5aaefdd37a85dd921c43a44c15a8
Keturah Walters <your.ketwalters@gmail.com>
2014-01-20 18:45:25 -0500

    remove to_a

--- todo40/seperate_numbers_with_commas.rb
+++ todo40/seperate_numbers_with_commas.rb
@@ -1,7 +1,7 @@
 #Separate numbers with a comma each three digits.
  
 def separate_with_comma(n)
-	array = n.to_a.to_s.split(//)
+	array = n.to_s.split(//)
 	if array.length > 3
 		array.insert(-4, ",")
 		array.join
@@ -10,6 +10,6 @@ def separate_with_comma(n)
 	end
 end
  
-puts separate_with_comma(1) #=> "1" 
-#separate_with_comma(1000) #=> "1,000" 
+#separate_with_comma(1) #=> "1" 
+puts separate_with_comma(1000) #=> "1,000" 
 #separate_with_comma(10000) #=> "10,000"
