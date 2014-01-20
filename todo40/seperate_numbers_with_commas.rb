da893c83ba84b572b29e0b60c3d18e4b5d71d67e
Keturah Walters <your.ketwalters@gmail.com>
2014-01-20 13:55:44 -0500

    add > 3 and else statment

--- todo40/seperate_numbers_with_commas.rb
+++ todo40/seperate_numbers_with_commas.rb
@@ -2,12 +2,14 @@
  
 def separate_with_comma(n)
 	array = n.to_a.to_s.split(//)
-	if array.length > 1
+	if array.length > 3
 		array.insert(-4, ",")
+		array.join
+	else
 		array.join
 	end
 end
  
-separate_with_comma(1) #=> "1" 
+puts separate_with_comma(1) #=> "1" 
 #separate_with_comma(1000) #=> "1,000" 
 #separate_with_comma(10000) #=> "10,000"
