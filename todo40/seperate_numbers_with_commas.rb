19820d7321f66fb05e4001be2f65388bd1dc0332
Keturah Walters <your.ketwalters@gmail.com>
2014-01-21 09:30:26 -0500

    add fizzbuzz js

--- todo40/seperate_numbers_with_commas.rb
+++ todo40/seperate_numbers_with_commas.rb
@@ -10,6 +10,6 @@ def separate_with_comma(n)
 	end
 end
  
-#separate_with_comma(1) #=> "1" 
-puts separate_with_comma(1000) #=> "1,000" 
+puts separate_with_comma(1) #=> "1" 
+#puts separate_with_comma(1000) #=> "1,000" 
 #separate_with_comma(10000) #=> "10,000"
